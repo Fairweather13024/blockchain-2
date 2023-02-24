a5780bce0836a93e4ba2e813068425a7f986ecb3
Mark Eric Fairweather <59223278+Fairweather13024@users.noreply.github.com>
2023-02-23 22:56:04 -0800

    create user in node storage

--- substrate-node-template-course/pallets/template/src/lib.rs
+++ substrate-node-template-course/pallets/template/src/lib.rs
@@ -32,15 +32,66 @@ pub mod pallet {
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 	}
 
+	///Implement custom struct
+	#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default, TypeInfo)]
+	pub struct UserInfo{
+		///user name stored as an array of bytes
+		pub username: Vec<u8>,
+		///number id of the user
+		pub id: i64,
+		///users about information
+		pub about_me: Vec<u8>,
+	}
+
+	///storage map to interact with the node's storage
+	#[pallet::storage]
+	#[pallet::getter(fn info)]
+	pub type AccountToUserInfo<T: Config> = StorageMap<_, Blake2_128Concat, T::AccountId, UserInfo, OptionQuery>;
+
+
+	///custom struct for a product in a supply chain
+	#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default, TypeInfo)]
+	pub struct Product {
+		pub id: u64,
+		pub name: Vec<u8>,
+		pub description: Vec<u8>,
+		pub owner: T::AccountId,
+		pub previous_owners: Vec<T::AccountId>,
+		pub timestamp: T::Moment,
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn products)]
+	pub type Products<T: Config> = StorageMap<_, Blake2_128Concat, u64, Product, OptionQuery>;
+
+
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/main-docs/build/events-errors/
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {}
+	pub enum Event<T: Config> {
+		UserCreated {user: T::AccountId},
+	}
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		/// The username is too long
+		UsernameTooLong,
+		/// The about me is too long
+		AboutMeTooLong,
+		/// The id is too small
+		IdTooSmall,
+		/// The id is too big
+		IdTooBig,
+		/// The product id already exists
+		ProductIdExists,
+		/// The product id does not exist
+		ProductIdNotFound,
+		/// The user is not the owner of the product
+		NotProductOwner,
+
+	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
 	// These functions materialize as "extrinsics", which are often compared to transactions.
@@ -48,5 +99,24 @@ pub mod pallet {
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
 		// Dispatchable calls go here!
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn register_user(origin: OriginFor<T>, username: Vec<u8>, id: i64, about_me: Vec<u8>) -> DispatchResult{
+			let sender = ensure_signed(origin)?;
+			ensure!(username.len() <= 32, Error::<T>::UsernameTooLong);
+			ensure!(about_me.len() <= 256, Error::<T>::AboutMeTooLong);
+			ensure!(id > 0, Error::<T>::IdTooSmall);
+			ensure!(id < 100000000, Error::<T>::IdTooBig);
+
+			let new_user = UserInfo{
+				username,
+				id,
+				about_me,
+			};
+
+			<AccountToUserInfo<T>>::insert(&sender, new_user);
+			Self::deposit_event(Event::UserCreated{user: sender});
+			Ok(())
 	}
+
+}
 }
